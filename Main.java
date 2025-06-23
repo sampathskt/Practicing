ecc1e807421201e45aa0bf801abddd04d8d589c0
Sampath Kumar T <Sampath.path98@gmail.com>
2025-06-23 19:08:40 +0530

    Add files via upload
    inheritance

--- Main.java
+++ Main.java
@@ -1,26 +1,27 @@
-/*Create a class called Book with:
-•	String title
-•	String author
-Use a parameterized constructor to initialize both variables.
-Add a method showBook() to print the book details.*/
-class Book
+/*A base class Animal with a method eat() that prints a message.
+1.	Create a subclass Dog that inherits from Animal and includes an additional method bark() that prints a message.
+2.	In a Main class, create an object of the Dog class and call both the eat() method (inherited from Animal) and the bark() method (defined in Dog).
+ANSWER:*/
+class Animal
 {
-    String title;
-    String author;
-    Book(String title, String author)
+    String a;
+    public void eat()
     {
-        this.title = title;
-        this.author = author;
+        System.out.println("eat the dog");
     }
-    public void showBook()
+}
+class Dog extends Animal
+{
+    String b;
+    public void bark()
     {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
+        System.out.println("bark the dog");
     }
 }
 class Main {
     public static void main(String[] args) {
-        Book skt = new Book("skt book", "skt skt");
-        skt.showBook();
+        Dog s1 = new Dog();
+        s1.eat();
+        s1.bark();
     }
 }
